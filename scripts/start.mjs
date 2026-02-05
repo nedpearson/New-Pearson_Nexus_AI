0252762583c1be2951464b720adc6c8a0c5961aa
nedpearson <nedpearson@gmail.com>
2026-02-04 19:47:13 -0600

    Squashed 'apps/pearson_nexus_ai_new/' content from commit 66b1d3a
    git-subtree-dir: apps/pearson_nexus_ai_new
    git-subtree-split: 66b1d3ac00ad4c5e0e9515be85babe96a9cc3a02

---
+++ scripts/start.mjs
@@ -0,0 +1,20 @@
+﻿import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const app = express();
+
+const distPath = path.join(__dirname, "../dist");
+
+app.use(express.static(distPath));
+
+app.get("*", (_, res) =>
+  res.sendFile(path.join(distPath, "index.html"))
+);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, "0.0.0.0", () => {
+  console.log("Server running on port", port);
+});
